bb7d8628b394e83dccd98cf0043a2a7ff256179d
Adriano <adriano.ianase@gmail.com>
2019-02-07 20:51:07 -0300

    Adicionada função de ordenação dos filmes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super();
+    this.orderById = this.orderById.bind(this);
+    this.orderByDate = this.orderByDate.bind(this);
+    this.order = this.order.bind(this);
+
     this.state = {
       movies: []
     }
@@ -19,6 +23,32 @@ class App extends React.Component {
     }
   }
 
+  order() {
+    const ordem = document.querySelector('#ordem');
+    if (ordem.value === 'id') {
+      this.orderById();
+    } else if (ordem.value === 'data') {
+      this.orderByDate();
+    }
+    ordem.value = "none"
+  }
+
+  orderById() {
+    this.setState((state) => {
+      const orderedMovies = state.movies.sort((a, b) => a['episode_id'] - b['episode_id']);
+      return { movies: orderedMovies };
+    })
+  }
+
+  orderByDate() {
+    this.setState((state) => {
+      const orderedMovies = state.movies.sort((a, b) => {
+        return Date.parse(a['release_date']) - Date.parse(b['release_date'])
+      });
+      return { movies: orderedMovies };
+    })
+  }
+
   componentDidMount() {
     this.getMovies();
   }
@@ -28,6 +58,11 @@ class App extends React.Component {
       <div className="App">
         <h1 className="f2 tc">STAR WARS</h1>
         <h2 className="f3 tc">Filmes</h2>
+        <select id="ordem" onChange={this.order}>
+          <option value='none'>Ordenar por</option>
+          <option value='id'>Cronologia da História</option>
+          <option value='data'>Data de Lançamento</option>
+        </select>
         <FolderList movies={this.state.movies} />
       </div>
     );
